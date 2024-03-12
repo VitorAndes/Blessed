16d5b54fd0fa540a04fc47e47614d7c49fa6b3f9
VitorAndes <vitor.andes.santos04@gmail.com>
2024-03-12 16:39:18 -0400

    mudança no header, animação e transição na seção #Sobre, mudança no link de contato

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,45 +1,56 @@
 import { Button } from "./ui/button";
-import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "./ui/navigation-menu";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "./ui/navigation-menu";
 import { BrowserRouter } from "react-router-dom";
-import { HashLink as Link} from "react-router-hash-link";
+import { HashLink as Link } from "react-router-hash-link";
 
-export function Header(){
-    return (
-            <div className="flex items-center justify-between p-8 bg-sky-500 rounded-md">
-                <h1 className="font-bold text-3xl text-black">BlessedFm.</h1>
-                <BrowserRouter>
-                <NavigationMenu className="hidden md:inline-block">
-                    <NavigationMenuList>
-                        <NavigationMenuItem>
-                            <Button type="button" variant='link' className="text-black/80 hover:text-slate-200 h-8">
-                                <NavigationMenuLink>
-                                    <Link to='#Sobre'>
-                                        Sobre
-                                    </Link>
-                                </NavigationMenuLink>
-                            </Button>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <Button type="button" variant='link' className="text-black/80 hover:text-slate-200 h-8">
-                                <NavigationMenuLink>
-                                    <Link to='#Produtos'>
-                                        Produtos
-                                    </Link>
-                                </NavigationMenuLink>
-                            </Button>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <Button type="button" variant='link' className="text-black/80 hover:text-slate-200 h-8">
-                                <NavigationMenuLink>
-                                    <Link to='#Contatos'>
-                                        Contatos
-                                    </Link>
-                                </NavigationMenuLink>
-                            </Button>
-                        </NavigationMenuItem>            
-                    </NavigationMenuList>
-                </NavigationMenu>
-                </BrowserRouter>
-            </div>
-    )
+export function Header() {
+  return (
+    <div className="flex items-center justify-between p-8 bg-sky-500/60 backdrop-blur-sm rounded-md">
+      <h1 className="font-bold text-3xl text-black">BlessedFm.</h1>
+      <BrowserRouter>
+        <NavigationMenu className="hidden md:inline-block">
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <Button
+                type="button"
+                variant="link"
+                className="text-black/80 hover:text-slate-200 h-8"
+              >
+                <NavigationMenuLink>
+                  <Link to="#Sobre">Sobre</Link>
+                </NavigationMenuLink>
+              </Button>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Button
+                type="button"
+                variant="link"
+                className="text-black/80 hover:text-slate-200 h-8"
+              >
+                <NavigationMenuLink>
+                  <Link to="#Produtos">Produtos</Link>
+                </NavigationMenuLink>
+              </Button>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Button
+                type="button"
+                variant="link"
+                className="text-black/80 hover:text-slate-200 h-8"
+              >
+                <NavigationMenuLink>
+                  <Link to="#Contatos">Contatos</Link>
+                </NavigationMenuLink>
+              </Button>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </BrowserRouter>
+    </div>
+  );
 }
--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,47 +1,61 @@
 import { Card, CardContent } from "./ui/card";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./ui/carousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
 import { getProducts } from "../data/products";
 import { useQuery } from "@tanstack/react-query";
-import Autoplay from "embla-carousel-autoplay"
+import Autoplay from "embla-carousel-autoplay";
 
-export function Products(){
+export function Products() {
+  const { data: products } = useQuery({
+    queryKey: ["products"],
+    queryFn: getProducts,
+  });
 
-    const {data: products} = useQuery({
-        queryKey:['products'],
-        queryFn:getProducts,
-    })
-
-    return(
-        <div className="flex flex-col items-center text-center space-y-3">
-            <div className="text-left space-y-3">
-                <h1 className="font-bold text-2xl  text-black">Produtos</h1>
-                <p className="font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas id laborum maiores itaque eius! Quis quaerat reiciendis iusto molestias officia incidunt, quod eaque dolores, inventore repellendus laudantium explicabo eveniet consequuntur!</p>
-            </div>
-            <div>
-                <Carousel
-                plugins={[
-                    Autoplay({
-                      delay: 3000,
-                    }),
-                  ]}
-                >
-                <CarouselContent className="md:w-[750px]">
-                {products?.map((product) => {
-                    return(
-                    <CarouselItem key={product.id} className='md:basis-1/2'>
-                            <Card>
-                                <CardContent className="flex aspect-square items-center mx-auto justify-center p-0">
-                                    <img className="h-[300px] w-[300px] rounded-2xl shadow-lg shadow-slate-600" src={product.img} alt={product.name} />
-                                </CardContent>
-                            </Card>
-                    </CarouselItem>
-                    )
-                })}
-                </CarouselContent>
-                <CarouselPrevious variant="default"/>
-                <CarouselNext variant="default"/>
-                </Carousel>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center text-center space-y-3">
+      <div className="text-left space-y-3">
+        <h1 className="font-bold text-2xl  text-black">Produtos</h1>
+        <p className="font-medium">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas id
+          laborum maiores itaque eius! Quis quaerat reiciendis iusto molestias
+          officia incidunt, quod eaque dolores, inventore repellendus laudantium
+          explicabo eveniet consequuntur!
+        </p>
+      </div>
+      <div>
+        <Carousel
+          plugins={[
+            Autoplay({
+              delay: 3000,
+            }),
+          ]}
+        >
+          <CarouselContent className="md:w-[750px]">
+            {products?.map((product) => {
+              return (
+                <CarouselItem key={product.id} className="md:basis-1/2">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center mx-auto justify-center p-0">
+                      <img
+                        className="h-[300px] w-[300px] rounded-2xl shadow-lg shadow-slate-600 cursor-grab active:cursor-grabbing"
+                        src={product.img}
+                        alt={product.name}
+                      />
+                    </CardContent>
+                  </Card>
+                </CarouselItem>
+              );
+            })}
+          </CarouselContent>
+          <CarouselPrevious variant="default" />
+          <CarouselNext variant="default" />
+        </Carousel>
+      </div>
+    </div>
+  );
 }
